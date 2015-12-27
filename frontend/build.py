10c8abd0664171d818ea1488e2e7769bbf257704
Anton Grbin <anton.grbin@gmail.com>
2015-12-27 11:00:14 +0000

    Add page for each concept
    This is a dummy implementation of a page.

--- frontend/build.py
+++ frontend/build.py
@@ -6,10 +6,24 @@ import sys, os
 import itertools
 import json
 
+def _makedirs(path):
+    try:
+        os.makedirs(path)
+    except OSError:
+        if not os.path.isdir(path):
+            raise
+
 def renderToFile(fname, template, **kwargs):
+    _makedirs(os.path.dirname(fname))
     f = open(fname, 'w')
     f.write(template.render(**kwargs).encode('utf-8'))
     f.close()
+
+def getBaseHref(fname):
+    result = ''
+    for null in fname.split('/')[:-1]:
+        result += '../'
+    return result
 
 if __name__ == '__main__':
     root_dir = sys.argv[1]
@@ -17,9 +31,19 @@ if __name__ == '__main__':
     gen_dir = root_dir + '/frontend/htdocs/'
     provider = ConceptProvider(root_dir)
 
+    concepts=provider.get_concepts()
+
     # Build index.html
     renderToFile(
-            '%s/index.html' % (gen_dir),
-            env.get_template('index.html'),
-            concepts=provider.get_concepts())
+        '%s/index.html' % (gen_dir),
+        env.get_template('index.html'),
+        concepts=concepts,
+        basehref='')
+
+    for concept in concepts:
+        renderToFile(
+            '%s/%s.html' % (gen_dir, concept.concept_name),
+            env.get_template('concept.html'),
+            concept=concept,
+            basehref=getBaseHref(concept.concept_name))
 
